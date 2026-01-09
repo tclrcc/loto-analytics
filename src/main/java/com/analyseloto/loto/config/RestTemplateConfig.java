f800a258e4070f5d1aee91b338b7080f7d98a011
Tony <tony.coloricchio@sully-group.fr>
2026-01-09 15:18:03 +0100

    rest template

--- src/main/java/com/analyseloto/loto/config/RestTemplateConfig.java
+++ src/main/java/com/analyseloto/loto/config/RestTemplateConfig.java
@@ -1,52 +1,64 @@
 package com.analyseloto.loto.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
+@Slf4j
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    public RestTemplate restTemplate() {
+        try {
+            // 1. On crée un contexte SSL de la manière la plus brute possible
+            // pour éviter que Java ne cherche le magasin "WINDOWS-ROOT"
+            SSLContext sslContext = SSLContext.getInstance("TLS");
 
-        // 1. Stratégie "Trust All" : On fait confiance à n'importe quel certificat
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            // On initialise avec un TrustManager qui accepte tout sans regarder le magasin système
+            sslContext.init(null, new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    }
+            }, new SecureRandom());
 
-        // 2. On construit un contexte SSL qui utilise cette stratégie
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
+            // 2. Configuration du client Apache
+            HttpClient httpClient = HttpClients.custom()
+                    .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
+                            .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
+                                    .setSslContext(sslContext)
+                                    .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                                    .build())
+                            .build())
+                    .build();
 
-        // 3. On configure le client Apache avec ce contexte permissif
-        HttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
-                        .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
-                                .setSslContext(sslContext)
-                                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE) // On ne vérifie même pas le nom de domaine
-                                .build())
-                        .build())
-                .build();
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            factory.setReadTimeout(5000);
+            factory.setConnectionRequestTimeout(5000);
 
-        // 4. On lie à Spring
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setReadTimeout(5000);
-        factory.setConnectionRequestTimeout(5000);
-
-        return new RestTemplate(factory);
+            return new RestTemplate(factory);
+        } catch (Exception e) {
+            // Au cas où ça échoue encore, on renvoie un RestTemplate basique pour ne pas bloquer le démarrage
+            log.error("❌ Impossible de configurer le SSL personnalisé, retour au mode dégradé", e);
+            return new RestTemplate();
+        }
     }
 }
