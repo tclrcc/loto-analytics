31923c751ed639e71875776f4eb22ca1ad8a0dd7
Tony <tony.coloricchio@sully-group.fr>
2026-01-26 20:10:09 +0100

    Serializable

--- src/main/java/com/analyseloto/loto/dto/StatsReponse.java
+++ src/main/java/com/analyseloto/loto/dto/StatsReponse.java
@@ -4,11 +4,15 @@ import com.analyseloto.loto.service.LotoService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class StatsReponse {
+public class StatsReponse implements Serializable {
+    @Serial private static final long serialVersionUID = 1L;
+
     private List<StatPoint> points; // Vos stats habituelles
     private String dateMin;
     private String dateMax;
