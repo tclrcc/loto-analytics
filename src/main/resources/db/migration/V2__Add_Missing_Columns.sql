3075aa8782dcb39861cd9e53ced0e957d1718783
Tony <tony.coloricchio@sully-group.fr>
2026-01-28 16:20:16 +0100

    Modif config flyway

--- src/main/resources/db/migration/V2__Add_Missing_Columns.sql
+++ src/main/resources/db/migration/V2__Add_Missing_Columns.sql
@@ -1,4 +1,6 @@
--- V1 : Ajout des colonnes pour le suivi de performance (ROI et Volume)
+-- V2 : Rattrapage et Ajout des colonnes ROI et Volume
+-- On utilise IF NOT EXISTS pour ne pas planter si la colonne est déjà là
+
 ALTER TABLE strategy_history
     ADD COLUMN IF NOT EXISTS nb_grilles_par_test INTEGER DEFAULT 50;
 
